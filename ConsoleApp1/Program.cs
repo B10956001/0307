a852c95e98d99d8692c16d20c748eb4ed0ef4931
B10956001 <b10956001@mail.npust.edu.tw>
2022-03-07 11:50:36 +0800

    finish

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,7 +6,13 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double meal_cost = 100;
+            int tip_percent = 15;
+            int tax_percent = 8;
+            int meal_cost_change = (int)meal_cost;
+            int total = meal_cost_change + tip_percent + tax_percent;
+            string total_cost = $"餐費總額為：{total}";
+            Console.WriteLine(total_cost);
         }
     }
 }
