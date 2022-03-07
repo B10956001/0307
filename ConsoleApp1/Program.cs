cbf53520b974d97a1bdd50e17b2229631d0e47cf
B10956001 <b10956001@mail.npust.edu.tw>
2022-03-07 11:01:24 +0800

    1.12

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,9 +7,6 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-
-            int test = 123;
-            Console.WriteLine("Hello World!");
         }
     }
 }
